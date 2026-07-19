1c6358750532bc47315bf2d04daaaaf8c726d200
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-19 16:05:02 +0530

    Pattern partice dsa

--- Patterns/Pattern4.java
+++ Patterns/Pattern4.java
@@ -1,19 +1,10 @@
-package Patterns;
-
-
-public class Pattern4 {
-    public static void main(String args[]) 
-    {for (int i=1;i<=6;i++){
-            int count=1;
-               
-            for(int j=1;j<=i-1;j++){
-               System.out.print(count);
-                
+class Pattern4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
             }
-         
             System.out.println();
         }
     }
-
-    
 }
