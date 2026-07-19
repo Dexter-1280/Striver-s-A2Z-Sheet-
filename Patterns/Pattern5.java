7d285afcc25d968fa7f4a294b13992fc778a59e3
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-19 16:30:45 +0530

    Pattern partice dsa

---
+++ Patterns/Pattern5.java
@@ -0,0 +1,13 @@
+
+
+public class Pattern5 {
+    public static void  main(String args[]){
+for(int i=1;i<=6;i++){
+    int n=5;
+    for(int j=1;j<=n-i+1;j++){
+        System.out.print("* ");
+    }
+    System.out.println();
+}
+    }
+}
