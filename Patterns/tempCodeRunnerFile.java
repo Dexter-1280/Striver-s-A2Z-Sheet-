f9f162342cd979c71c1a7e85d2e5ce87744e24fe
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-20 18:17:27 +0530

    Pattern partice dsa

---
+++ Patterns/tempCodeRunnerFile.java
@@ -0,0 +1,18 @@
+public class Pattern12 {
+    public static void main(String args[]){
+        int n=6;
+         int count=1;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(count);
+            }
+            for(int j=1;j<2*n-2*i;j++){
+                System.out.println();
+            }
+            for(int j=1;j>=1i;j--){
+                System.out.print(count);
+            }
+        }
+
+    }
+}
