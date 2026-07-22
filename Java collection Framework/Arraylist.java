3d1cfd4069504ef8cc41b02ae9e364b9cf00aa03
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-22 16:02:00 +0530

    stiver a to z

---
+++ Java collection Framework/Arraylist.java
@@ -0,0 +1,14 @@
+
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist {
+    public static void main(String args[]){
+        //List or collection->interface
+        //arraylist->concreate class
+        ArrayList<Integer> arr=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        Collection<Integer> collection=new ArrayList<>();
+    }
+}
