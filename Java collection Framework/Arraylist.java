710b14bd776f2faefbdf3383774bf4b5f7eb53ce
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-22 16:08:37 +0530

    stiver a to z

--- Java collection Framework/Arraylist.java
+++ Java collection Framework/Arraylist.java
@@ -13,8 +13,17 @@ public class Arraylist {
         arr.add(20);
         arr.add(30);
         System.out.println(arr);
-        
+       arr.add(50);
+       //remove the element
+       arr.remove(2); 
+       System.out.println(arr);
+
         List<Integer> list=new ArrayList<>();
+        list.add(9);
+System.out.println(list);
+//to add all 
+list.addAll(arr);
+System.out.println(list);
         Collection<Integer> collection=new ArrayList<>();
     }
 }
