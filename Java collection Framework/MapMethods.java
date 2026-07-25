d2585c8791c2615bf847af1cd50d721b3eafc0f9
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-25 18:31:10 +0530

    stiver a to z java collection

---
+++ Java collection Framework/MapMethods.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class MapMethods {
+    public static void main(String args[]){
+        //map java collection 
+        Map<String,String> mapping=new HashMap<>();
+        //insertion 
+        //Map java collection is used as key --> value
+        mapping.put("in", "India");
+        mapping.put("in","india2"); 
+    }
+}
