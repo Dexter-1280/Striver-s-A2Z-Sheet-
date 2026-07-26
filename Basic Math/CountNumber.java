acc65b4a6fc8db660f0a4618e4c08676fd2fb3f8
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-26 16:01:41 +0530

    Basic math solution

---
+++ Basic Math/CountNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CountNumber {
+   
+ public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter your input values:");
+String num = sc.next();
+System.out.println(num);
+System.out.println(num.length());
+sc.close();
+ }    
+}
