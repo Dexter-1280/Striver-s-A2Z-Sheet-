785e347d38fbb23feb6b3602e30d1fe21287298d
Aditya Deshmukh <adityadeshmukh628@gmail.com>
2026-07-27 19:42:27 +0530

    Basic math solution

--- Basic Math/Palidrome.java
+++ Basic Math/Palidrome.java
@@ -1,13 +1,22 @@
 import java.util.*;
 
 public class Palidrome {
-      static isPalidrome(int x){
+      static boolean isPalidrome(int x){
+        int rev=0;
+        if(x<0) return false;
+        int originalNum=x;
+       int num=x%10;
+        x=x/10;
+        rev=rev*10+num;
 
+        return originalNum==rev ;
         }
         
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        
+        int x=sc.nextInt();
+        System.out.println("Enter your value:");
       isPalidrome(x);
+      sc.close();
     }
 }
